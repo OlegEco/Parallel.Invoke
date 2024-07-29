5f7e70983f32450fb292ed45c1bd00707d295a1b
Oleg <100606637+OlegEco@users.noreply.github.com>
2024-07-29 16:29:53 +0200

    Add project files.

---
+++ Parallel.Invoke/Program.cs
@@ -0,0 +1,14 @@
+﻿Parallel.Invoke(
+    () => Task1(),
+    () => Task2(),
+    () => Task3()
+    );
+Console.WriteLine("All tasks completed.");
+
+
+static void Task1() => Console.WriteLine($"Task 1 is running on thread {Task.CurrentId}");
+
+
+static void Task2() => Console.WriteLine($"Task 2 is running on thread {Task.CurrentId}");
+
+static void Task3() => Console.WriteLine($"Task 3 is running on thread {Task.CurrentId}");
